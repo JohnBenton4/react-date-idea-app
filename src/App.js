7b13143bc28e9b1e691a5be79a1bb118feb5d86f
John Benton <jtbentoniv@gmail.com>
2023-04-07 13:11:23 -0400

    additional instructions

--- src/App.js
+++ src/App.js
@@ -1,63 +1,81 @@
-import './App.css';
-import NumberList from './components/NumberList/NumberList';
-import TypeList from './components/TypeList/TypeList';
-import Search from './components/Search/Search';
-import PriceList from './components/PriceList/PriceList';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { useEffect, useState } from 'react';
+import "./App.css";
+import NumberList from "./components/NumberList/NumberList";
+import TypeList from "./components/TypeList/TypeList";
+import Search from "./components/Search/Search";
+import PriceList from "./components/PriceList/PriceList";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState } from "react";
+import { NavLink } from "react-bootstrap";
 
 function App() {
-
-  const [type, setType] = useState({})
-  const [participants, setParticipants] = useState({})
-  const [price, setPrice] = useState({})
-  const [isTypeDisabled, setIsTypeDisabled] = useState(false)
-  const [isParticipantsDisabled, setIsParticipantsDisabled] = useState(false)
-  const [isPriceDisabled, setIsPriceDisabled] = useState(false)
+  const [type, setType] = useState({});
+  const [participants, setParticipants] = useState({});
+  const [price, setPrice] = useState({});
+  const [isTypeDisabled, setIsTypeDisabled] = useState(false);
+  const [isParticipantsDisabled, setIsParticipantsDisabled] = useState(false);
+  const [isPriceDisabled, setIsPriceDisabled] = useState(false);
 
   useEffect(() => {
     if (type.activity) {
-      setIsParticipantsDisabled(true)
-      setIsPriceDisabled(true)
+      setIsParticipantsDisabled(true);
+      setIsPriceDisabled(true);
     }
 
     if (participants.activity) {
-      setIsTypeDisabled(true)
-      setIsPriceDisabled(true)
+      setIsTypeDisabled(true);
+      setIsPriceDisabled(true);
     }
 
     if (price.activity) {
-      setIsTypeDisabled(true)
-      setIsParticipantsDisabled(true)
+      setIsTypeDisabled(true);
+      setIsParticipantsDisabled(true);
     }
-  },
-
-  [participants.activity, price.activity,type.activity]
-  );
+  }, [participants.activity, price.activity, type.activity]);
 
   return (
     <div className="container">
-      <div><h1 className="title">Date Idea App</h1></div>
+      <div>
+        <h1 className="title">Date Idea App</h1>
+      </div>
+      <br></br>
+      <div>Follow this link to receive temporary access before continuing:</div>
+      <div> <a href= "https://cors-anywhere.herokuapp.com/corsdemo">Temporary Access</a></div>
       <div className="searchFields">
         <br></br>
-        <div className="type"><h2> What Type Of Date Are You Looking For? </h2>
-          <TypeList onSetType={setType} type={type} isDisabled={isTypeDisabled} />
+        <div className="type">
+          <h2> What Type Of Date Are You Looking For? </h2>
+          <TypeList
+            onSetType={setType}
+            type={type}
+            isDisabled={isTypeDisabled}
+          />
         </div>
-        <div className="number"><h2> How Many People Are Going on This Date? </h2>
-          <NumberList onSetParticipants={setParticipants} participants={participants} isDisabled={isParticipantsDisabled} />
+        <div className="number">
+          <h2> How Many People Are Going on This Date? </h2>
+          <NumberList
+            onSetParticipants={setParticipants}
+            participants={participants}
+            isDisabled={isParticipantsDisabled}
+          />
         </div>
-        <div className="price"><h2> How Much Do You Want To Spend? </h2>
-          <PriceList onSetPrice={setPrice} price={price} isDisabled={isPriceDisabled} />
+        <div className="price">
+          <h2> How Much Do You Want To Spend? </h2>
+          <PriceList
+            onSetPrice={setPrice}
+            price={price}
+            isDisabled={isPriceDisabled}
+          />
         </div>
       </div>
       <br></br>
-      <Search activity={type.activity || participants.activity || price.activity} />
+      <Search
+        activity={type.activity || participants.activity || price.activity}
+      />
     </div>
   );
 }
 
 export default App;
 
-
 //the parent controls the state, the parent can send it to the child and the child receives it
-//typelist send it back up to App.jss and Search receives the info
+//typelist sends it back up to App.jss and Search receives the info
